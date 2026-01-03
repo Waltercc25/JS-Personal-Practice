608881f8c972f44535958630be9ab7326863b3d5
Waltercc25 <cantorclaros@gmail.com>
2026-01-03 17:09:33 -0600

    Adding parcial deep copy example

---
+++ Array-Methods/ShallowVSDeepCopy/Practice1.js
@@ -0,0 +1,52 @@
+//Shallow copy vs Deep copy 
+
+//learning about how to modify nested data without mutating the original.
+
+//commun error, where we want to change the theme from light to dark.
+const user = {
+    id: 1,
+    name: 'walter',
+    settings: {
+        theme: 'light',
+        Notifications: true
+    }
+};
+
+
+//Incorrect way, we accidently mutate the object.
+/*const updated1 = { ...user};
+updated1.settings.theme = 'dark';*/
+
+//Correct way (parcial deep copy).
+const updated2 = {
+    ...user,
+    settings: {
+        ...user.settings,
+        theme: 'dark'
+    }
+};
+
+console.log(user.settings.theme);     // "light"
+console.log(updated2.settings.theme);  // "dark"
+
+
+//Changing the user email without mutating.
+const user2 = {
+    id: 2,
+    profile: {
+        email: "old@gmail.com",
+        phone: "123456"
+    }
+};
+
+const updated3 = {
+    ...user2,
+    profile: {
+        ...user2.profile,
+        email: "new@mail.com"
+    }
+};
+
+
+
+//
